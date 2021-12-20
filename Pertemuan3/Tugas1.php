9151b9145cd30759b652b2c6b33c1a7df23470e5
bangbrayens <ryanltcsn@gmail.com>
2021-12-20 13:31:37 +0700

    menambahkan file pertemuan 3

---
+++ Pertemuan3/Tugas1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Web Programming</title>
+</head>
+<body>
+    <h1>Tugas Web Programming 5B</h1>
+    <h2>Muhamad Briyan Latuconsina</h2>
+    <h2>1904030012</h2>
+        <?php
+            $namadpn = "Muhamad Briyan";
+            $namablk = "Latuconsina";
+            $namapgl = "Bray";
+            $umur = (0x14);
+            $ttl = "Tangerang, 02-05-2001";
+            $alamat = "Perum Dasana Indah blok si 14 no.19";
+            $hobi = "Nonton Film";
+            $Mknfvrt = "Sate";
+            $Mnfvrt = "Es Kelapa Muda";
+            $Flmfvrt = "Arrival";
+        ?>
+    <hr>
+    <h2>Identitas Diri</h2>
+    <p>Nama : <?php echo $namadpn ." ". $namablk; ?></p>
+    <p>Nama Panggilan : <?php echo $namapgl; ?></p>
+    <p>Umur : <?php echo $umur; ?></p>
+    <p>Tempat Tanggal Lahir : <?php echo $ttl; ?></p>
+    <p>Alamat : <?php echo $alamat; ?></p>
+    <p>Hobi : <?php echo $hobi; ?></p>
+    <p>Makanan Favorit : <?php echo $Mknfvrt; ?></p>
+    <p>Minuman Favorit : <?php echo $Mnfvrt; ?></p>
+    <p>Film Favorit : <?php echo $Flmfvrt; ?></p>
+
+</body>
+</html>
